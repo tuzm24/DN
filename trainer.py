fb1be5f78f5d90ea501019b2e421789cd6004956
YangwooKim <tuzm24@naver.com>
2020-05-21 17:45:11 +0900

    extenstion bug fix

--- trainer.py
+++ trainer.py
@@ -40,7 +40,7 @@ class Trainer():
         # TEMP
         self.loader_train.dataset.set_scale(0)
         for batch, (lr, hr, ext, _) in enumerate(self.loader_train):
-            if not (len(ext.shape)==1 or torch.isnan(ext[0])):
+            if not (len(ext.shape)==1 and torch.isnan(ext[0])):
                 lr[0] = torch.cat((lr[0], ext), axis=1)
             lr, hr = self.prepare(lr, hr)
             timer_data.hold()
@@ -88,7 +88,7 @@ class Trainer():
             for idx_scale, scale in enumerate(self.scale):
                 d.dataset.set_scale(idx_scale)
                 for lr, hr, ext, filename in tqdm(d, ncols=80):
-                    if not (len(ext.shape)==1 or torch.isnan(ext[0])):
+                    if not (len(ext.shape)==1 and torch.isnan(ext[0])):
                         lr[0] = torch.cat((lr[0], ext), axis=1)
                     lr, hr = self.prepare(lr, hr)
                     sr = self.model(lr[0], idx_scale)
